2dcb46d6e455a411fa8317dd9cc0e06f1c4e37a9
diego <dieguito.reyes.14xd@gmail.com>
2026-03-28 23:01:25 -0600

    protegimisrutas_diego_segunrol

---
+++ src/views/DashboardPersonal.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useAuthStore } from '@/stores/auth'
+const authStore = useAuthStore()
+</script>
+
+<template>
+  <div style="padding: 20px; text-align: center;">
+    <h2> Bienvenido de nuevo PERSONAL </h2>
+   
+    <p>Ya estás dentro del sistema de la estética.</p>
+  </div>
+</template>
