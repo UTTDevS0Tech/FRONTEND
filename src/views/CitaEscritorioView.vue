ca713e9dcb58dac61c16efa6c1c44d7170c1cf9f
Meredith Judith Valdivia Hernandez <24170085@uttcampus.edu.mx>
2026-04-07 09:49:44 -0600

    pinche c en minuscula pendejo

--- src/views/CitaEscritorioView.vue
+++ src/views/CitaEscritorioView.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
-import CitaEscritorioForm from '@/components/CitaEscritorioForm.vue'
+import CitaEscritorioForm from '@/components/citaEscritorioForm.vue'
 import { useTipoServiciosStore } from '@/stores/tipoServicios'
 import { useCitaEscritorioStore } from '@/stores/citaEscritorio'
 import { useClienteStore } from '@/stores/cliente'
