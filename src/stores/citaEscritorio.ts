83ad8a0d223f57b3beca1244806f44fe0971cc00
Carlos D <carlosdanielgrdz@gmail.com>
2026-04-08 16:49:27 -0600

    o fak

--- src/stores/citaEscritorio.ts
+++ src/stores/citaEscritorio.ts
@@ -1,6 +1,6 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
-import { useApiCitaEscritorio } from '@/composables/UseApiCitaEscritorio'
+import { useApiCitaEscritorio } from '@/composables/useApiCitaEscritorio'
 import type {
   CitaEscritorioPayload,
   CitaEscritorioResponse,
