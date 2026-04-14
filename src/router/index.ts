06046f12158a95732a922bfa5fffd2d11a2207b7
Meredith Judith Valdivia Hernandez <24170085@uttcampus.edu.mx>
2026-04-13 23:44:15 -0600

    xd

--- src/router/index.ts
+++ src/router/index.ts
@@ -145,7 +145,7 @@ const router = createRouter({
     {
     path: '/dashboard/estilista/citas',
     name: 'estilista-citas',
-   component: () => import('@/components/EstilistasCitasView.vue') 
+   component: () => import('@/views/EstilistasCitasView.vue') 
     }
 
 
